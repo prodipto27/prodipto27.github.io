ca576e0f4ada2c2b81960f756c206b052c82e183
prodipto27 <70103196+prodipto27@users.noreply.github.com>
2021-01-07 12:05:28 +0600

    Added PDF viewer for syllabus and books

---
+++ js/book-list-generator.js
@@ -0,0 +1,17 @@
+var bookname, edition;
+var htmlString = '';
+
+function generateBookList(books, editions, credits){
+    var listParent = document.getElementsByClassName("mdc-list")[0];
+
+    for(var i = 0; i < books.length; i++){
+        bookname = books[i];
+        edition = editions[i];
+
+        htmlString += '<li onclick="openBook(\'' + bookname + '\', ' + i + ')" class="mdc-list-item" ' + ((i == 0) ? 'tabindex="0"' : '') + '><span class="mdc-list-item__ripple"></span><span class="mdc-list-item__text"><span class="mdc-list-item__primary-text">' + bookname + '</span><span class="mdc-list-item__secondary-text">' + edition + '</span></span></li>';    
+    }
+
+    listParent.innerHTML = htmlString;
+}
+
+generateBookList(books, editions, credits);
