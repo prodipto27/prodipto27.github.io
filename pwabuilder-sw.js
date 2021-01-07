eddd3fd53ea9f15ed530ff5e002791d7df1503b9
prodipto27 <70103196+prodipto27@users.noreply.github.com>
2021-01-07 13:44:57 +0600

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network
 
-const CACHE = "prodipto27-v0.3.3";
+const CACHE = "prodipto27-v0.3.4";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
