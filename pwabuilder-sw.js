e685ebb2c72419c6dd46044696182b0173a96f76
ms-jahan <ssarwarjahan@gmail.com>
2021-09-16 12:06:29 +0600

    Version change

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network
 
-const CACHE = "prodipto27-v0.7.1";
+const CACHE = "prodipto27-v0.7.2";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
