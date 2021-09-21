5b0aa1e315ee1529b5c69f3a84f21be38aa95217
ms-jahan <ssarwarjahan@gmail.com>
2021-09-22 00:57:51 +0600

    Changed routine and added some
    '

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network
 
-const CACHE = "prodipto27-v0.7.2";
+const CACHE = "prodipto27-v0.7.2-iausghai";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
