2c03e98b8f328c8c3b0ac99b798290d92913bae9
ms-jahan <ssarwarjahan@gmail.com>
2022-01-23 17:31:19 +0000

    Updated links for 3rd Semester.

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network
 
-const CACHE = "prodipto27-v0.7.6-asdf43";
+const CACHE = "prodipto27-v0.7.6-asderw4";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
