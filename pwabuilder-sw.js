f0c24b581352989bf94b5c68f0390888315a4c75
Md. Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2023-08-02 00:38:24 +0600

    Updated Manifest

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network
 
-const CACHE = "prodipto27-v0.8.5-34rsse";
+const CACHE = "prodipto27-v0.8.5-werwe";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
